d02498ccd783744c77f44104b766c2b1a472a98f
Daud Zoss <daudzoss@gmail.com>
2017-03-03 14:54:23 -0800

    more work on demo app

--- demo_zos.asm
+++ demo_zos.asm
@@ -3,14 +3,20 @@ include p16f1847.inc
 	
 __CONFIG
 
-;;; uncomment to reduce zOS footprint by 100 words (at cost of FRK/EXE/FND SWI):
-;;;zOS_MIN equ 1
+;;; uncomment to reduce zOS footprint by 100 words (at cost of zOS_FRK/EXE/FND):
+;
+zOS_MIN	equ	1
+	
 include zos.inc
 include zosmacro.inc
+	
+;;; uncomment to pre-load stack positions with indices (for debugging ZOS_ROL):
+;
+	zOS_DBG
 
 ;;; while SWI handlers normally know what line the interupts will come in on,
 ;;; for flexibility of incorporation into any application this choice is not
-;;; hardwired into zosmacro.inc and any available line may be chosen:
+;;; hardwired into zosmacro.inc library and any available line may be chosen:
 OUTCHAR	equ	zOS_SI3
 	
 	zOS_MON	1,9600,PORTA,RA0
@@ -18,15 +24,15 @@ OUTCHAR	equ	zOS_SI3
 	zOS_ARG	3
 retry1
 	zOS_SWI	zOS_NEW
-	btfsc	STATUS,Z	; zOS_MON(/*SSP*/1,9600,PORTA,RA0/*hbeat*/);
+	btfsc	STATUS,Z	; zOS_MON(/*SSP*/1, 9600, PORTA, RA0/*hbeat*/);
 	bra	retry1		; zOS_ARG(3, OUTCHAR);//handles without knowing!
 	
 	movlw	low spitjob	; do {} while (zOS_SWI(zOS_NEW) == 0); // 1?
-	movwf	FSR0L		; fsr0 = spitjob; // no interrupts
-	movlw	high spitjob	; zOS_ARG(0,0);
-	movwf	FSR0H		; zOS_ARG(1,0);
-	clrw			; zOS_ARG(2,0);
-	nop			; zOS_ARG(3,0);
+	movwf	FSR0L		; fsr0 = spitjob; // takes no interrupts
+	movlw	high spitjob	; zOS_ARG(0, 0);
+	movwf	FSR0H		; zOS_ARG(1, 0);
+	clrw			; zOS_ARG(2, 0);
+	nop			; zOS_ARG(3, 0);
 	zOS_ARG	0
 	zOS_ARG	1
 	zOS_ARG	2
@@ -40,11 +46,13 @@ retry3
 	btfsc	STATUS,Z	; zOS_RUN();
 	bra	retry3		;}
 	zOS_RUN
+	
 spitjob	
-	movf	BSR,w		;void spitjob(void) {
+	movf	zOS_ME		;void spitjob(void) {
 	addlw	'0'		; do {
 	zOS_ARG	0
 	zOS_SWI	OUTCHAR
+#if 0
 i	equ	0x20
 j	equ	0x21
 loop
@@ -54,6 +62,7 @@ loop
 	incf	i,f		;   for (int j = 0; j & 0xff; j++)
 	btfss	STATUS,Z	;    ;
 	bra	loop		; } while (1);
+#endif
 	bra	spitjob		;}
 	
 	end
