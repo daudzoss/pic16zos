090f39d4a9b272302341bfe0114dfbae7bf606fb
Daud Zoss <daudzoss@gmail.com>
2017-03-09 18:59:14 -0800

    ready to start setting up serial port speed, wrote some macros for string output via EECON

--- demo_zos.asm
+++ demo_zos.asm
@@ -18,7 +18,7 @@ zOS_MIN	equ	1
 ;;; for flexibility of incorporation into any application this choice is not
 ;;; hardwired into zosmacro.inc library and any available line may be chosen:
 	
-	zOS_CON	1,9600,PIR1,PORTA,RA0
+	zOS_CON	1,9600,PIR1,PORTB,RB5
 OUTCHAR	equ	zOS_SI3
 	movlw	OUTCHAR		;void main(void) {
 	zOS_ARG	3
@@ -28,9 +28,9 @@ retry1
 	btfsc	STATUS,Z	; zOS_MON(/*SSP*/1,9600,PIR1,PORTA,RA0/*beat*/);
 	bra	retry1		; zOS_ARG(3, OUTCHAR);//handles without knowing!
 	
-	movlw	low spitjob	; do {} while (zOS_SWI(zOS_NEW) == 0);
-	movwf	FSR0L		; fsr0 = spitjob & 0x7fff;
-	movlw	high spitjob	; zOS_ARG(0, 0);
+	movlw	low splash	; do {} while (zOS_SWI(zOS_NEW) == 0);
+	movwf	FSR0L		; fsr0 = splash & 0x7fff;
+	movlw	high splash	; zOS_ARG(0, 0);
 	andlw	0x7f		; zOS_ARG(1, 0);
 	movwf	FSR0H		; zOS_ARG(2, 0);
 	clrw			; zOS_ARG(3, 0);// takes no interrupts
@@ -42,17 +42,37 @@ retry2
 	zOS_SWI	zOS_NEW
 	movf	WREG,w		;
 	btfsc	STATUS,Z	; do {} while (zOS_SWI(zOS_NEW) == 0); // 2?
-	bra	retry2		; do {} while (zOS_SWI(zOS_NEW) == 0); // 3?
+	bra	retry2		;
+	
+	movlw	low spitjob	; do {} while (zOS_SWI(zOS_NEW) == 0);
+	movwf	FSR0L		; fsr0 = spitjob & 0x7fff;
+	movlw	high spitjob	; zOS_ARG(0, 0);
+	andlw	0x7f		; zOS_ARG(1, 0);
+	movwf	FSR0H		; zOS_ARG(2, 0);
+	clrw			; zOS_ARG(3, 0);// FIXME: needs interrupt to GO!
+	zOS_ARG	0
+	zOS_ARG	1
+	zOS_ARG	2
+	zOS_ARG 3
 retry3
 	zOS_SWI	zOS_NEW
 	movf	WREG,w		;
+	btfsc	STATUS,Z	; do {} while (zOS_SWI(zOS_NEW) == 0); // 2?
+	bra	retry3		; do {} while (zOS_SWI(zOS_NEW) == 0); // 3?
+retry4
+	zOS_SWI	zOS_NEW
+	movf	WREG,w		;
 	btfsc	STATUS,Z	; zOS_RUN(/*T0IE in*/INTCON, /*T0IF in*/INTCON);
-	bra	retry3		;}
+	bra	retry4		;}
 	
 	banksel	OPTION_REG
 	bcf	OPTION_REG,T0CS	; OPTION_REG &= ~(1<<TMR0CS);// off Fosc not pin
 ;;FIXME: set the prescaler appropriately so that the IRQ frequency isn't crazy
 	zOS_RUN	INTCON,INTCON
+	
+splash
+	zOS_STR	OUTCHAR,"hello!\r\n"
+	zOS_SWI	zOS_END
 	
 spitjob	
 #if 0
