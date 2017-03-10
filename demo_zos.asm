abf324bca5ad5d26cc982adf6b84af6636a6fa34
Superuser <Superuser>
2017-03-09 22:25:38 -0800

    mostly finished with message-passing and cleaned up inits/strings

--- demo_zos.asm
+++ demo_zos.asm
@@ -25,11 +25,11 @@ OUTCHAR	equ	zOS_SI3
 	zOS_LAU	WREG		; zOS_ARG(3, OUTCHAR);//handles without knowing!
 	
 	zOS_INT	0,0		; zOS_INT(0,0);//no interrupt handler for splash
-	zOS_ADR	splash -zOS_PRV	; zOS_ADR(splash -zOS_PRV); // unprivileged job
+	zOS_ADR	splash -zOS_PRV	; zOS_ADR(fsr0 = splash -zOS_PRV);//unprivileged
 	zOS_LAU	FSR1L		; zOS_LAU(&fsr1); // stash ID in FSR1L until end
 	
 	zOS_INT	0,0		; zOS_INT(0,0);//no interrupt handler either
-	zOS_ADR	spitjob -zOS_PRV; zOS_ADR(spitjob -zOS_PRV);// unprivileged jobs
+	zOS_ADR	spitjob -zOS_PRV; zOS_ADR(fsr0 = spitjob-zOS_PRV);//unprivileged
 	zOS_LAU	FSR1H		; zOS_LAU(1 + &fsr1); // launch two copies...
 	zOS_LAU	WREG		; zOS_LAU(&w);// remembering job# in FSR1H, WREG
 	
@@ -51,12 +51,22 @@ OUTCHAR	equ	zOS_SI3
 ;;;FIXME: set the prescaler appropriately so that the IRQ frequency isn't crazy
 	zOS_RUN	INTCON,INTCON	; zOS_RUN(/*T0IE in*/INTCON, /*T0IF in*/INTCON);
 	nop			;}
+
+put_str
+	zOS_STR	OUTCHAR,""	;void put_str(const char*){zOS_STR(OUTCHAR,"");}
+	return			;
 	
+greet
+	da	"Demo application for zOS"
+crlf
+	da	"\r\n",0
 splash
-	zOS_MY2	FSR0		;void splash(void) {
-	zOS_STR	OUTCHAR,"Hi\r\n"; zOS_MY2(&fsr0);
-	movlw	1		; zOS_STR(OUTCHAR, "Hi\r\n");
-	comf	INDF0,f		; *fsr0 = 1; // will cause spitjob()s to unstick
+	zOS_MY2	FSR1		;void splash(void) {
+	zOS_ADR	greet
+	pagesel	put_str		; zOS_MY2(&fsr1);
+	call	put_str		; zOS_ADR(fsr0 ="Demo application for zOS\r\n");
+	movlw	1		; put_str(fsr0);
+	movwf	INDF1		; *fsr1 = 1; // will cause spitjob()s to unstick
 #if 0
 	movf	zOS_ME ;implicit
 	zOS_ARG	0
@@ -69,45 +79,48 @@ spitjob
 	moviw	0[FSR0],w	;void splash(void) {
 	movwf	FSR1L		;
 	moviw	1[FSR0],w	; zOS_MY2(&fsr0);
-	movwf	FSR1H		; fsr1 = *fsr0;
-	movf	zOS_ME		; zOS_ME(&w); // shouldn't be clobbered by below
+	movwf	FSR1H		; fsr1 = *fsr0; // watch fsr1 for nonzero
+	movf	zOS_ME		; w = zOS_ME(); // shouldn't get clobbered below
 awaitgo
-#if 0
-	zOS_ARG	0
-	;; being nice is optional
-	zOS_SWI	zOS_YLD		;
+#if 1
+	zOS_ARG	0;implicit since nonprivileged
+	;;being nice is optional:
+	zOS_SWI	zOS_YLD
 #endif	
-	movf	INDF1,f		;
-	btfss	awaitgo		;
-	
-	btfss
-
-
-#if 0
-	clrw			;// if even, print as hexadecimal integer: OUTCHAR 0 int
+	movf	INDF1,f		; do {
+	btfss	STATUS,Z	;
+	bra	awaitgo		;  do {} while (*fsr1 == 0);
+	andlw	1		;
+	brw			;  switch (w & 1) {
+	bra	asxbyte		; 
+	bra	asascii		;  case 0:
+asxbyte
+	clrw			;   zOS_ARG(0, 0);
 	zOS_ARG	0
-#else	
-	movlw	'0'		;// if odd, print as ascii digit: OUTCHAR '0'+int
-#endif	
-	addwf	zOS_ME		;void spitjob(void) { do {
-#if 0
+	movf	zOS_ME		;   zOS_ARG(1, w);
 	zOS_ARG	1
-#else
-	zOS_ARG	0
-#endif
-	zOS_SWI	OUTCHAR
-#if 0
+	bra	print		;   break;
+asascii
+	movlw	'0'		;  case 1:
+	addwf	zOS_ME		;   zOS_ARG(0, w);
+	zOS_ARG	0		;  }
+print
+	zOS_SWI	OUTCHAR		;  zOS_SWI(OUTCHAR);
+	zOS_ADR	crlf		;  zOS_ADR(fsr0 = "\r\n");
+	pagesel	put_str	
+	call	put_str		;  put_str(fsr0);
+#if 1
 i	equ	0x20
 j	equ	0x21
 loop
-	incf	j,f		;  zOS_ARG(0,'0'+bsr); // job number
-	btfss	STATUS,Z	;  zOS_SWI(OUTCHAR);
+	incf	j,f		;
+	btfss	STATUS,Z	;
 	bra	loop		;  for (int i = 0; i & 0xff; i++)
 	incf	i,f		;   for (int j = 0; j & 0xff; j++)
 	btfss	STATUS,Z	;    ;
 	bra	loop		; } while (1);
 #endif
-	bra	spitjob		;}
+	bra	awaitgo		;}
 	
 	end
 	
