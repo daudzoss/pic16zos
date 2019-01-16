2b902b161232dd31a927e3a14c124e762f28c450
Daud Zoss <daud.zoss@dexcom.com>
2019-01-16 11:34:54 -0800

    preambl4.inc and its generation tool now includable

--- tools/802.15.6/preambl4.c
+++ tools/802.15.6/preambl4.c
@@ -4,8 +4,9 @@
  *  with the Table 85 orthogonal codes required of the FS-spreader by 802.15.6
  */
 
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
   static const uint16_t table85[16] = { 0xffff, // 0000
@@ -27,15 +28,17 @@ int main(void) {
 					0xa55a, // 1101
 					0xc33c, // 1110
 					0x9669};// 1111
+  int page, nyb_h, nyb_l, offset = 0;
 
-  for (int page = 0; page < 4; page++)
-    for (int nyb_h = 0; nyb_h < 16; nyb_h++)
-      for (int nyb_l = 0; nyb_l < 16; nyb_l++) {
+  for (page = 0; page < 4; page++)
+    for (nyb_h = 0; nyb_h < 16; nyb_h++)
+      for (nyb_l = 0; nyb_l < 16; nyb_l++) {
 
 	uint32_t code = (table85[nyb_h] << 16) | table85[nyb_l];
-	uint8_t byte = 0x000000ff & (code >> (8 * (3 - page)));
+	uint8_t byte = 0x000000ff & (code >> (8 * page));
 
-	printf("\tmovlw\t0x%2x\n", byte);
+	printf("\tmovlw\t0x%02x\t;offset 0x%03x (page %d, %x followed by %x)\n",
+	       byte, offset++, page, nyb_h, nyb_l);
       }
   exit(0);
 }
