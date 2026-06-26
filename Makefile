1c4272e1085620593e709c53f42b01cee64ee492
Daud Zoss <daud.zoss@gmail.com>
2026-06-25 20:02:08 -0700

    getting demo_zos to build again

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
+INSTALLDIr=/opt/microchip/xc8/v3.00
+INCPATH=$(INSTALLDIR)/pic/include/proc
+ASSEMBLER=$(INSTALLDIR)/pic-as/bin/pic-as
+ASMFLAGS=-Wa,-a -save-temps -xassembler-with-cpp
+
+
 demo_zos.hex: demo_zos.asm zos.inc zosmacro.inc
-	  gpasm demo_zos.asm | grep -v 1302
+	  $(ASSEMBLER) -I$(INCPATH) $(ASMFLAGS) -mcpu=16f1719 $< -o $@
 
 demo_802.hex: demo_802.asm zos.inc zosmacro.inc zos80215.inc
-	  gpasm demo_802.asm | grep -v 1302
+	  $(ASSEMBLER) -I$(INCPATH) $(ASMFLAGS) -mcpu=16f1619 $< -o $@
 
 olirelay.hex: olirelay.asm zos.inc zosmacro.inc
-	  gpasm olirelay.asm | grep -v 1302
+	  $(ASSEMBLER) -I$(INCPATH) $(ASMFLAGS) -mcpu=16f1847 $< -o $@
 
--- demo_zos.asm
+++ demo_zos.asm
@@ -5,43 +5,43 @@
 ;;;
 ;;; after starting job #1 as a console output buffer (zOS_CON() in zosmacro.inc)
 ;;; to demonstrate privileged mode (able to kill or otherwise tweak other tasks)
-;;; 
+;;;
 ;;; two final processes (initially numbered jobs 3 and 4) run in re-entrant
 ;;; functions dummy and dummy2
-;;; 
+;;;
 ;;; if fewer than the 5 possible job slots are used, as in this demo, reducing
 ;;; the max allowed value to 4 or lower will waste less time in the scheduler:
 ;zOS_NUM	equ	4
 
 	processor 16f1719
-	include p16f1719.inc
-	
-	__CONFIG _CONFIG1,_FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _CP_OFF & _BOREN_ON & _CLKOUTEN_ON & _IESO_ON & _FCMEN_ON
-	__CONFIG _CONFIG2,_WRT_OFF & _PPS1WAY_OFF & _ZCDDIS_ON & _PLLEN_ON & _STVREN_ON & _BORV_LO & _LPBOR_OFF & _LVP_ON
+#include <pic16f1719.inc>
+
+;FIXME;	__CONFIG _CONFIG1,_FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _CP_OFF & _BOREN_ON & _CLKOUTEN_ON & _IESO_ON & _FCMEN_ON
+;FIXME;	__CONFIG _CONFIG2,_WRT_OFF & _PPS1WAY_OFF & _ZCDDIS_ON & _PLLEN_ON & _STVREN_ON & _BORV_LO & _LPBOR_OFF & _LVP_ON
 
 ;;; uncomment to reduce zOS footprint by 100 words (at cost of zOS_FRK/EXE/FND):
 ;zOS_MIN	equ	1
-	
-	include zos.inc
-	include zosmacro.inc
-	
+
+#include <zos.inc>
+#include <zosmacro.inc>
+
 OUTCHAR	equ	zOS_SI3
-	
+
 ;;; uncomment to pre-load stack positions with indices (for debugging ZOS_ROL):
 ;	zOS_DBG
 
-main
+main:
 	banksel OSCCON			;{
-	movlw	0x70		;    // SCS FOSC; SPLLEN disabled; IRCF 8MHz_HF; 
+	movlw	0x70		;    // SCS FOSC; SPLLEN disabled; IRCF 8MHz_HF;
 	movwf	OSCCON		;    OSCCON = 0x70;
-	movlw	0x80		;    // SOSCR enabled; 
+	movlw	0x80		;    // SOSCR enabled;
 	movwf	OSCSTAT		;    OSCSTAT = 0x80;
-	movlw	0x00		;    // TUN 0; 
+	movlw	0x00		;    // TUN 0;
 	movwf	OSCTUNE		;    OSCTUNE = 0x00;
 				;    // Wait for PLL to stabilize
-	btfss	OSCSTAT,PLLR	;    while(PLLR == 0)
+	btfss	PLLR		;    while(PLLR == 0)
 	bra	$-1		;     ;
-	
+
 	banksel	ANSELA
 	movlw	0xaf		;
 	movwf	ANSELA		; ANSELA = 0xaf; // allow heartbeat GPIO, CLKOUT
@@ -49,12 +49,12 @@ main
 	movwf	ANSELC		; ANSELC = 0x3c; // allow serial port
 
 	banksel	OPTION_REG
-	bcf	OPTION_REG,PSA	; OPTION_REG &= ~(1<<PSA);// max timer0 prescale
-	bcf	OPTION_REG,T0CS	; OPTION_REG &= ~(1<<TMR0CS);// off Fosc not pin
+	bcf	PSA		; OPTION_REG &= ~(1<<PSA);// max timer0 prescale
+	bcf	T0CS		; OPTION_REG &= ~(1<<TMR0CS);// off Fosc not pin
 
 	banksel	TRISC
-	bcf	TRISA,RA4	; TRISA &= ~(1<<RA4); // allow heartbeat output
-;	bcf	TRISA,RA6	; TRISA &= ~(1<<RA6); // allow clock output
+	bcf	TRISA4		; TRISA &= ~(1<<RA4); // allow heartbeat output
+;	bcf	TRISA6		; TRISA &= ~(1<<RA6); // allow clock output
 	movlw	0x7f
 	movwf	TRISC
 
@@ -63,10 +63,10 @@ main
 	movwf	PPSLOCK
 	movlw	0xaa
 	movwf	PPSLOCK
-	bcf	PPSLOCK,PPSLOCKED
+	bcf	PPSLOCKED
 	movlw	0x16
 	movwf	RXPPS
-	
+
 	banksel	RC7PPS
 	movlw	0x14
 	movwf	RC7PPS
@@ -74,34 +74,34 @@ main
 	movwf	PPSLOCK
 	movlw	0xaa
 	movwf	PPSLOCK
-	bsf	PPSLOCK,PPSLOCKED
+	bsf	PPSLOCKED
 
 ;;; while SWI handlers normally know what line the interrupts will come in on,
 ;;; for flexibility of incorporation into any application this choice is not
 ;;; hardwired into zosmacro.inc library and any available line may be chosen:
 ;	zOS_MAN	0,.32000000/.9600,PIR1,LATA,RA4,0
-	zOS_CLC	0,.32000000/.9600,PIR1,LATA,RA4,0
+;FIXME;	zOS_CLC	0,.32000000/.9600,PIR1,LATA,RA4,0
 	movlw	OUTCHAR		;void main(void) {
 	movwi	0[FSR0]		; zOS_xxx(/*UART*/1,32MHz/9600bps,PIR1,LATA,4);
-	
+
 	zOS_INT	0,0
 	zOS_ADR	dummy,zOS_UNP
 	zOS_LAU	WREG
-	zOS_ACT	FSR0
+	zOS_ACT	FSR0L
 
 	zOS_INT	0,0
 	zOS_ADR	dummy2,zOS_UNP
 	zOS_LAU	WREG
-	zOS_ACT	FSR0
+	zOS_ACT	FSR0L
 
 	zOS_RUN	INTCON,INTCON	; zOS_RUN(/*T0IE in*/INTCON, /*T0IF in*/INTCON);
-	
+
 	zOS_NAM	"infinite loop"
-dummy
+dummy:
 	bra	dummy
 
 	zOS_NAM	"cooperative loop"
-dummy2
+dummy2:
 	movf	zOS_ME
 	zOS_ARG	0
 	zOS_SWI	zOS_YLD
