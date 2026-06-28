965afa415574003938bc17c14cc4d0965c2ea924
Daud Zoss <dzoss@lunadiabetes.com>
2026-06-27 18:21:02 -0700

    demo_zos.asm builds with zOS_MAN too now

--- demo_zos.asm
+++ demo_zos.asm
@@ -83,8 +83,11 @@ main:
 ;;; while SWI handlers normally know what line the interrupts will come in on,
 ;;; for flexibility of incorporation into any application this choice is not
 ;;; hardwired into zosmacro.inc library and any available line may be chosen:
-;	zOS_MAN	0,.32000000/.9600,PIR1,LATA,RA4,0
-;FIXME;	zOS_CLC	0,.32000000/.9600,PIR1,LATA,RA4,0
+#if 1
+	zOS_MAN	0,32000000/9600,PIR1,LATA,4,0
+#else
+	zOS_CLC	0,32000000/9600,PIR1,LATA,4,0
+#endif
 	movlw	OUTCHAR		;void main(void) {
 	movwi	0[FSR0]		; zOS_xxx(/*UART*/1,32MHz/9600bps,PIR1,LATA,4);
 
